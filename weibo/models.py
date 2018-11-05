20e61baba83705093e2e6c7b32d3c61b624014d1
Xiang Chaozhu <XiangCZ929@Gmail.com>
2018-11-05 23:20:23 +0800

    custom UserModels

--- weibo/models.py
+++ weibo/models.py
@@ -38,12 +38,11 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    username = models.CharField()
+    username = models.CharField('Username', max_length=30, unique=True)
     email = models.EmailField('Email address', unique=True)
     password = models.CharField(max_length=16)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     objects = UserManager()
